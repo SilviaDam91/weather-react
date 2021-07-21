a3c6529603753646b92fd124ead62b680ac23c02
SilviaDam91 <silvia.pariota@gmail.com>
2021-07-21 13:04:25 +0200

    added Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
+import "bootstrap/dist/css/bootstrap.css";
 import Weather from "./Weather";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Weather city="Rome" />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <Weather city="Rome" />
+        </header>
+      </div>
     </div>
   );
 }
